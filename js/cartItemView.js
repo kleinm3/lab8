22e4349fc3dffc2eb3753af2e0a4cd594e2602b8
unknown <iguest@is-m430m27-27.ischool.uw.edu>
2013-11-15 09:23:36 -0800

    info 343

--- js/cartItemView.js
+++ js/cartItemView.js
@@ -7,5 +7,18 @@
 */
 
 function createCartItemView(config) {
-
+	var view = createTemplateView(config);
+	view.afterRender = function(clonedTemplate, model) {
+    clonedTemplate.find('.remove-item').click(function(){
+        view.cartModel.removeItem(model);
+    });
+    config.cartModel = config.model;
+	config.templateView = createCartItemView(config);
+	var view = createTemplateListView(config);
+	view.afterRender = function() {
+	this.totalPrice.html(this.model.getTotalPrice());
+	}; //afterRender()
+};
+return view;
+                    
 } //createCartItemView()
